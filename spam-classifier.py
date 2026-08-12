45a950e50bf3aba37d076b6a1c409bd1cfe27554
Majeownsall1 <128427957+Majeownsall1@users.noreply.github.com>
2026-08-12 11:48:51 +0000

    split data

--- spam-classifier.py
+++ spam-classifier.py
@@ -1,6 +1,31 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
-
+#get data 
 data = pd.read_csv("data/sample-message.csv")
 
-print(data)
+
+X = data["text"]
+y = data["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.25,
+    random_state=42
+)
+
+print("training messages:")
+print(X_train)
+
+print("testing mesages:")
+print(X_test)
+
+print("training labels:")
+print(y_train)
+
+print("testing labels:")
+print(y_test)
