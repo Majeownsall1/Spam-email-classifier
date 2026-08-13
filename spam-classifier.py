6df883b75e9eb712019615e78df2c0dfdbe0e51b
Majeownsall1 <128427957+Majeownsall1@users.noreply.github.com>
2026-08-13 23:11:50 +0000

    added accuracy score

--- spam-classifier.py
+++ spam-classifier.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
 #get data 
 data = pd.read_csv("data/sample-message.csv")
@@ -15,8 +16,18 @@ X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
     test_size=0.25,
-    random_state=42
+    random_state=42,
+    stratify=y
 )
+
+model = Pipeline([
+    ("vectorizer", CountVectorizer()),
+    ("classifier", MultinomialNB())
+])
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
 
 print("training messages:")
 print(X_train)
@@ -29,3 +40,11 @@ print(y_train)
 
 print("testing labels:")
 print(y_test)
+
+print("Real answers:")
+print(y_test.values)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("accuracy:")
+print(accuracy)
